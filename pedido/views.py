172f8578bb1a23490a020a11810f3c65083440ab
Carol Genari <carolinegenari@gmail.com>
2023-04-14 13:30:21 +0100

    first commit

---
+++ pedido/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from django.views import View
+
+
+class Pagar(View):
+    pass
+
+
+class SalvarPedido(View):
+    pass
+
+
+class Detalhe(View):
+    pass
